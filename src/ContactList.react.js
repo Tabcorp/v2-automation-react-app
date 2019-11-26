c44c97d77f2f9f8b58fe7d0b9a7b225c2b8527b1
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2019-11-27 10:31:29 +1100

    added hidden div

--- src/ContactList.react.js
+++ src/ContactList.react.js
@@ -47,6 +47,8 @@ export class ContactList extends React.Component {
     return (
       <div className="ContactList">
         <div className="ContactListSearch" key="search">
+          <div className="hidden-div" data-id="hidden-div"></div>
+          <div className="hidden-div" data-id="hidden-div"></div>
           <input onChange={this.onSearchChange} data-id="search" placeholder="Search" />
           <button className="ContactListAdd" data-id="add" onClick={this.props.onAddClick}>
             Add
